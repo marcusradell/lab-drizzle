38ea56c72af0fe5385d147eccbd028d7bc076202
Marcus Rådell <marcus@radell.net>
2024-07-19 21:09:05 +0200

    implement GET /posts/counts route

--- src/features/posts/router.ts
+++ src/features/posts/router.ts
@@ -4,7 +4,15 @@ import { Service } from "./service";
 export const createRouter = (service: Service) => {
   const router = Router();
 
-  // TODO: implement routes.
+  router.get("/counts", async (req, res, next) => {
+    try {
+      const result = await service.getPostsCounts();
+
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  });
 
   return router;
 };
