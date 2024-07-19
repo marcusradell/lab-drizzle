234232f9d7c5a0e8f938e18bb41eaaecae471ca9
Marcus Rådell <marcus@radell.net>
2024-07-19 19:20:08 +0200

    implement get user by id route

--- src/features/users/router.ts
+++ src/features/users/router.ts
@@ -5,7 +5,19 @@ export const createRouter = (service: Service) => {
   const router = Router();
 
   router.get("/", async (req, res) => {
-    const result = service.getUsers(1, 100);
+    const result = await service.getUsers(1, 100);
+
+    res.json(result);
+  });
+
+  router.get("/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      throw new Error("Invalid ID from URL.");
+    }
+
+    const result = await service.getUserById(id);
 
     res.json(result);
   });
