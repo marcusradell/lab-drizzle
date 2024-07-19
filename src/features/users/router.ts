fab9645cc021e15ed024f63904a3a47201bd67dd
Marcus Rådell <marcus@radell.net>
2024-07-19 18:36:14 +0200

    add getUsers route

--- src/features/users/router.ts
+++ src/features/users/router.ts
@@ -4,5 +4,11 @@ import { Service } from "./types";
 export const createRouter = (service: Service) => {
   const router = Router();
 
+  router.get("/", async (req, res) => {
+    const result = service.getUsers(1, 100);
+
+    res.json(result);
+  });
+
   return router;
 };
