5f30b683ba6be3159271069381c3d94650262b7c
Marcus Rådell <marcus@radell.net>
2024-07-19 21:33:04 +0200

    implement test for getting empty list of users

---
+++ src/features/users/router/router.test.ts
@@ -0,0 +1,17 @@
+import { describe, it } from "node:test";
+import request from "supertest";
+import { createApp } from "../../../app";
+import { deepEqual } from "node:assert/strict";
+import { config } from "dotenv";
+
+describe("users router", () => {
+  config({ path: ".env.local" });
+
+  it("gets users", async () => {
+    const { expressApp } = createApp();
+
+    const response = await request(expressApp).get("/api/v1/users");
+
+    deepEqual(response.body, []);
+  });
+});
