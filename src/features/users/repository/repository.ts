6a4bb209b5f0f8c96a8fc8ae1404c5206a59c4ba
Marcus Rådell <marcus@radell.net>
2024-07-19 17:11:30 +0200

    extract users repository from barrel file

---
+++ src/features/users/repository/repository.ts
@@ -0,0 +1,10 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import * as schema from "./schema";
+import { createQueries } from "./queries";
+import { DbClient } from "../../../db-client";
+
+export const createRepository = (dbClient: DbClient) => {
+  const db = drizzle(dbClient, { schema });
+
+  return createQueries(db);
+};
