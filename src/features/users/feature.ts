d48493f50089662d7b8fb04d20b045210be67ec0
Marcus Rådell <marcus@radell.net>
2024-07-19 18:25:33 +0200

    extract users feature to its own file

---
+++ src/features/users/feature.ts
@@ -0,0 +1,13 @@
+import { DbClient } from "../../db-client";
+import { createRepository } from "./repository";
+import { createRouter } from "./router";
+import { createService } from "./service";
+
+export const createUsersFeature = (dbClient: DbClient) => {
+  const repository = createRepository(dbClient);
+
+  const service = createService(repository);
+  const router = createRouter(service);
+
+  return { service, router };
+};
