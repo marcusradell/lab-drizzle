292bf02feac250f93b47c49b52f4f6f6ad234814
Marcus Rådell <marcus@radell.net>
2024-07-19 15:54:20 +0200

    add back default page and page size

--- src/features/users/service.ts
+++ src/features/users/service.ts
@@ -1,7 +1,7 @@
 import { Repository } from "./repository";
 
 export const createService = (repository: Repository) => {
-  const getUsers = async (page: number, pageSize: number) => {
+  const getUsers = async (page = 1, pageSize = 5) => {
     return repository.getUsers(page, pageSize);
   };
 
